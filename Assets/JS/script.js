24e8d5f6bddea4e8bb4849f3319949402c50074d
Jobayer75368 <jobayer30325@gmail.com>
2026-01-15 00:33:06 +0600

    mobile sideBar added

---
+++ Assets/JS/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+    $(".sideBar_btn").click(function () {
+        $(".mobile_sideBar").toggleClass("mobile_sideBar_open");
+    });
+    $(".sideBar_close").click(function () {
+        $(".mobile_sideBar").toggleClass("mobile_sideBar_open");
+    });
+});
