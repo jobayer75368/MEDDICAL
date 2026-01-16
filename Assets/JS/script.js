95d292652bf71d4c9e5f9c12bce7b191c04bf6db
Jobayer75368 <jobayer30325@gmail.com>
2026-01-16 11:06:36 +0600

    scroll fix

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,28 +1,21 @@
 $(document).ready(function () {
+
     $(".sideBar_btn").click(function () {
         $(".mobile_sideBar").toggleClass("mobile_sideBar_open");
     });
+
     $(".sideBar_close").click(function () {
         $(".mobile_sideBar").toggleClass("mobile_sideBar_open");
     });
 
-    $("header")
     $(window).scroll(function () {
-        if ($(window).scrollTop() > 60) {
-            $("header").css("transform", "translateY(-65%)");
+        if ($(window).width() <= 768) {
+            if ($(window).scrollTop() > 60) {
+                $("header").css("transform", "translateY(-70%)");
+            } else {
+                $("header").css("transform", "translateY(0)");
+            }
         }
-        else {
-            $("header").css("transform", "translateY(0)")
-        }
-    })
+    });
+
 });
-
-// const topmost = document.querySelector("header");
-
-// window.addEventListener("scroll", function () {
-//     if (window.scrollY > 60) {
-//         topmost.style.transform = "translateY(-100%)";
-//     } else {
-//         topmost.style.transform = "translateY(0)";
-//     }
-// });
