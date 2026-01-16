dd9a36b2c01d6add1fa8c2e44d1ebf147f57c1b7
Jobayer75368 <jobayer30325@gmail.com>
2026-01-16 10:10:20 +0600

    JS added

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -5,4 +5,24 @@ $(document).ready(function () {
     $(".sideBar_close").click(function () {
         $(".mobile_sideBar").toggleClass("mobile_sideBar_open");
     });
+
+    $("header")
+    $(window).scroll(function () {
+        if ($(window).scrollTop() > 60) {
+            $("header").css("transform", "translateY(-65%)");
+        }
+        else {
+            $("header").css("transform", "translateY(0)")
+        }
+    })
 });
+
+// const topmost = document.querySelector("header");
+
+// window.addEventListener("scroll", function () {
+//     if (window.scrollY > 60) {
+//         topmost.style.transform = "translateY(-100%)";
+//     } else {
+//         topmost.style.transform = "translateY(0)";
+//     }
+// });
